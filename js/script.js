9fe7bc44756e5e6846fbe7076b07e07e40728066
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-04-04 19:58:09 +0300

    FRont end

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  $('.navbar-toggler').blur(function(){
+
+    if ($(window).width() < 960) {
+      alert('Less than 960');
+      $('#navbarSupportedContent').hide(500, console.log("working"));
+    } else {
+      alert('More than 960');
+    }
+  });
+  $('.fader').fadeIn("3000");
+
+  $(window).scroll(function(){
+    if ($(window).width() < 960) {
+      let scrollBottom = $(window).scrollTop();
+      if (scrollBottom > 30){
+        $("#brandc").animate({
+          left: '10%',
+        }, 800);
+      }
+    } else {
+
+    }
+  });
+
+});
+
+
+// window.onbeforeunload = function() {
+//   return "Two buttons will be below this message asking if the user wants to stay on this page or leave.";
+// }
